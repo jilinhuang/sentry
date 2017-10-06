997becf57a1951f6ebdec589c0e282d5df6d7def
Jess MacQueen <jess@getsentry.com>
2017-10-06 16:10:37 -0700

    pr feedback

--- src/sentry/filters/preprocess_hashes.py
+++ src/sentry/filters/preprocess_hashes.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from django.core.cache import cache, get_cache, InvalidCacheBackendError
 
+from sentry.models import FilteredGroupHash
 
 try:
     hash_cache = get_cache('preprocess_hash')
@@ -11,3 +12,10 @@ except InvalidCacheBackendError:
 
 def get_raw_cache_key(project_id, event_id):
     return 'e:raw:{1}:{0}'.format(project_id, event_id)
+
+
+def matches_discarded_hash(data, project):
+    return FilteredGroupHash.objects.filter(
+        project_id=project,
+        hash__in=get_preprocess_hashes(data),
+    ).exists()
